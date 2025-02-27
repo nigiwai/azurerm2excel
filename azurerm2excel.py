e63069bf277fd9592c55761b1babe3bc09f5a21c
kkanno3@clouddxcenter02.onmicrsoft.com <kensuke.kanno@biprogy.com>
2025-02-27 23:00:33 +0900

    sheet名変更

--- azurerm2excel.py
+++ azurerm2excel.py
@@ -212,8 +212,8 @@ def write_to_excel(resources_by_type, descriptions, output_folder):
 
                 # application_rule_collectionだけ別のシートに出力
                 if application_rule_collections:
-                    ws_appcol = wb.create_sheet(title=f"_application_rule_collection")
-                    ws_appcolrule = wb.create_sheet(title=f"_application_rule")
+                    ws_appcol = wb.create_sheet(title=f"apcol_{sheet_title}")
+                    ws_appcolrule = wb.create_sheet(title=f"aprule_{sheet_title}")
                     # Add header row for application_rule_collection sheet
                     ws_appcol.append(
                         [
@@ -324,8 +324,8 @@ def write_to_excel(resources_by_type, descriptions, output_folder):
 
                 # network_rule_collectionだけ別のシートに出力
                 if network_rule_collections:
-                    ws_netcol = wb.create_sheet(title=f"_network_rule_collection")
-                    ws_netcolrule = wb.create_sheet(title=f"_network_rule")
+                    ws_netcol = wb.create_sheet(title=f"nwcol_{sheet_title}")
+                    ws_netcolrule = wb.create_sheet(title=f"nwrule_{sheet_title}")
                     # Add header row for network_rule_collection sheet
                     ws_netcol.append(
                         [
@@ -429,8 +429,8 @@ def write_to_excel(resources_by_type, descriptions, output_folder):
                 
                 # nat_rule_collectionだけ別のシートに出力
                 if nat_rule_collections:
-                    ws_natcol = wb.create_sheet(title=f"_nat_rule_collection")
-                    ws_natcolrule = wb.create_sheet(title=f"_nat_rule")
+                    ws_natcol = wb.create_sheet(title=f"natcol_{sheet_title}")
+                    ws_natcolrule = wb.create_sheet(title=f"natrule_{sheet_title}")
                     # Add header row for nat_rule_collection sheet
                     ws_natcol.append(
                         [
